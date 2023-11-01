13591c02fa887f3956dda788d926afb6e1bc16d5
Shabna610 <shaikshabna2023@gmail.com>
2023-11-01 16:41:35 -0500

    made changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var brandsRouter = require('./routes/brands');
+var brandsRouter = require('./routes/brand');
 var boardRouter = require('./routes/board');
 
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/brands', brandsRouter);
+app.use('/brand', brandsRouter);
 app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
